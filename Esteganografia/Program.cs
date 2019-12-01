afb778e623d94deb47d34f42edb15bdb2a7b660c
Oskar <oskarcali18@gmail.com>
2019-12-01 15:15:49 -0600

    Finish program
    Fin del programa

--- Esteganografia/Program.cs
+++ Esteganografia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Esteganografia.Views;
 
 namespace Esteganografia
 {
@@ -16,7 +17,7 @@ namespace Esteganografia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formHome());
         }
     }
 }
